ecad0265ed7674527e771d23db2f27a6da9dc02a
Jem Clift <clifj025.309@lgflmail.net>
2020-08-14 18:01:38 +0000

    v3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var board_size = [28,28]; // [y,x]
+var board_size = [9,9]; // [y,x]
 var mines = 10;
 
 function generateGrid() {
@@ -36,7 +36,7 @@ function calculateHints(board) {
       try { if (board[y-1][x+1] == "x") { mine_count++ } } catch (e) {}
 
       try { if (board[y][x-1] == "x") { mine_count++ } } catch (e) {}
-      try { if (board[y-1][x+1] == "x") { mine_count++ } } catch (e) {}
+      try { if (board[y][x+1] == "x") { mine_count++ } } catch (e) {}
 
       try { if (board[y+1][x-1] == "x") { mine_count++ } } catch (e) {}
       try { if (board[y+1][x] == "x") { mine_count++ } } catch (e) {}
@@ -57,16 +57,64 @@ function generateCompleteBoard() {
   return board;
 }
 
-function drawTiles() {
+function drawTiles(board) {
+  var boardElement = document.getElementById("board")
+  while (boardElement.firstChild) {
+    boardElement.removeChild(boardElement.lastChild);
+  }
   for (y=0; y<board_size[1]; y++) {
     for (x=0; x<board_size[0]; x++) {
       var tile = document.createElement("div");
       tile.id = "tile"
       tile.className = "unopened"
-      document.getElementById("board").appendChild(tile)
-      document.getElementById("board").style.width = (24*board_size[1]+6)+"px"
+      tile.onclick = function() {digTile(this,board)};
+      tile.addEventListener('contextmenu', function(el) {
+          el.preventDefault();
+          flagTile(this);
+          return false;
+      }, false);
+      tile.row = y;
+      tile.column = x;
+      boardElement.appendChild(tile)
+      boardElement.style.width = (24*board_size[1]+6)+"px"
     }
   }
 }
 
-drawTiles()
+function digTile(tile,board) {
+  if (tile.className != "flagged") {
+    tile.className = "opened";
+    var text = "";
+    if (board[tile.row][tile.column] == "x") {
+      tile.className = "first-mine"
+      alert('game over');
+    } else if (board[tile.row][tile.column] > 0) {
+      text = board[tile.row][tile.column]
+      uncoverTiles(tile.row, tile.column);
+    }
+    tile.innerHTML = text;
+  }
+}
+
+function uncoverTiles(row, column, board) {
+  if (board[row][column] == "0") {
+    document.g
+  } else {
+    // look in all direcrions
+  }
+}
+
+function flagTile(tile) {
+  if (tile.className == "unopened") {
+    tile.className = "flagged";
+  } else if (tile.className == "flagged") {
+    tile.className = "unopened"
+  }
+}
+
+function newGame() {
+  var board = generateCompleteBoard();
+  drawTiles(board);
+}
+
+newGame();
